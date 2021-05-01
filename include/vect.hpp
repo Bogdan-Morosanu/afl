136819262b88e5f1f86ac6e040a33448c38a2876
Bogdan Morosanu <bogdan.moro.mail@gmail.com>
2021-05-01 21:49:35 +0200

    First version

---
+++ include/vect.hpp
@@ -0,0 +1,47 @@
+/*
+ * vect.hpp
+ *
+ *  Created on: Feb 7, 2016
+ *      Author: moro
+ */
+
+#ifndef INCLUDE_VECT_HPP_
+#define INCLUDE_VECT_HPP_
+
+#include "mat.hpp"
+
+namespace afl {
+
+  /// @brief template for vector (we assume column vectors).
+  template <typename ElType, int dim>
+  class Vect final : public Mat<ElType, dim, 1> {
+  public:
+    Vect (ElType val)
+      : Mat<ElType, dim, 1>(val) {}
+
+    Vect (const std::initializer_list<ElType>& ls)
+      : Mat<ElType, dim,1 >(ls) {}
+
+    /// @brief returns projection of *this on rhs.
+    inline Vect
+      projected(const Vect& rhs) const;
+
+    /// @brief returns vector of length one,
+    /// pointing in same direction as *this
+    inline Vect
+      normalized() const;
+
+    /// @brief returns component of *this which is
+    /// orthogonal to axis given.
+    inline Vect
+      orthogonalized(const Vect& axis) const;
+
+    /// TODO decide if virtual dtor is needed ?
+    virtual ~Vect () {}
+  };
+
+} /* namespace afl */
+
+#include "vect-inl.hpp"
+
+#endif /* INCLUDE_VECT_HPP_ */
